bca29928b02fe9740ccaed387234c69937643f17
Ahmed Elbasha <ahmedelbasha@macbooks-MacBook-Pro.local>
2017-12-27 00:50:48 +0200

    GoalCell is built

---
+++ Post-Goal/Post-Goal/View/GoalCell.swift
@@ -0,0 +1,34 @@
+//
+//  GoalCell.swift
+//  Post-Goal
+//
+//  Created by Ahmed Elbasha on 12/26/17.
+//  Copyright © 2017 Ahmed Elbasha. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+    
+    @IBOutlet weak var goalDescriptionLabel: UILabel!
+    @IBOutlet weak var goalTypeLabel: UILabel!
+    @IBOutlet weak var goalProgressLabel: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+    func configureCell(description: String, type: String, goalProgressAmount: Int) {
+        self.goalDescriptionLabel.text = description
+        self.goalTypeLabel.text = type
+        self.goalProgressLabel.text = String(describing: goalProgressAmount)
+    }
+}
