b15f85bd0f1194b09d204d97bc82b6fe2b3500f6
Ahmed Elbasha <33283612+Ahmed-Elbasha@users.noreply.github.com>
2018-01-14 15:51:24 +0200

    Added extensions for UIView and UIButton classes and applying it

--- Post-Goal/Post-Goal/Controller/CreateGoalViewController.swift
+++ Post-Goal/Post-Goal/Controller/CreateGoalViewController.swift
@@ -15,8 +15,13 @@ class CreateGoalViewController: UIViewController {
     @IBOutlet weak var longTermButton: UIButton!
     @IBOutlet weak var nextButton: UIButton!
     
+    var goalType: GoalType = .shortTerm
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        nextButton.bindToKeyboard()
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -25,12 +30,18 @@ class CreateGoalViewController: UIViewController {
     }
     
     @IBAction func longTermButtonWasPressed(_ sender: Any) {
+        goalType = .longTerm
+        longTermButton.setSelectedColor()
+        shortTermButton.setDeselectedColor()
     }
     
     @IBAction func nextButtonWasPressed(_ sender: Any) {
     }
     
     @IBAction func shortTermButtonWasPressed(_ sender: Any) {
+        goalType = .shortTerm
+        shortTermButton.setSelectedColor()
+        longTermButton.setDeselectedColor()
     }
     
     @IBAction func returnButtonWasPressed(_ sender: Any) {
